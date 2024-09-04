5ed84fa63b63584db3a02b4a85ec4ed115a0c91c
djap1221 <djap1221@naver.com>
2024-09-04 13:46:21 +0900

    [feat] get pig details by pigId

--- backend/src/main/java/com/example/donzoom/controller/PigController.java
+++ backend/src/main/java/com/example/donzoom/controller/PigController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +34,13 @@ public class PigController {
     }
   }
 
+  @GetMapping("/{pigId}")
+  public ResponseEntity<PigResponseDto> getPigById(@PathVariable Long pigId) {
+    try {
+      PigResponseDto pigResponse = pigService.getPigById(pigId);
+      return ResponseEntity.ok(pigResponse);
+    }catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // 잘못된 요청인 경우
+    }
+  }
 }
--- backend/src/main/java/com/example/donzoom/service/PigService.java
+++ backend/src/main/java/com/example/donzoom/service/PigService.java
@@ -4,6 +4,7 @@ import com.example.donzoom.dto.pig.response.PigResponseDto;
 import com.example.donzoom.entity.MyPig;
 import com.example.donzoom.entity.Pig;
 import com.example.donzoom.repository.MyPigRepository;
+import com.example.donzoom.repository.PigRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,9 @@ import org.springframework.stereotype.Service;
 public class PigService {
 
   private final MyPigRepository myPigRepository;
+  private final PigRepository pigRepository;
 
+  //지갑에있는 돼지 정보 보기
   public List<PigResponseDto> getPigs(long walletId) {
     List<MyPig> myPigs = myPigRepository.findByWallet_WalletId(walletId);
     return myPigs.stream()
@@ -26,5 +29,17 @@ public class PigService {
             .collectionName(myPig.getPig().getCollectionName())
             .build())
         .collect(Collectors.toList());
+  }
+
+  //돼지 아이디로 돼지 상세보기
+  public PigResponseDto getPigById(Long pigId) {
+    Pig pig = pigRepository.findById(pigId)
+        .orElseThrow(() -> new IllegalArgumentException("Pig not found with id: " + pigId));
+
+    return PigResponseDto.builder()
+        .collectionId(pig.getCollectionId())
+        .imageUrl(pig.getImageUrl())
+        .collectionName(pig.getCollectionName())
+        .build();
   }
 }
