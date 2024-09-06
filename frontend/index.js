8e1e92ca1d33855b51a98fe40e9a3df8f25fef87
aprldpdy289 <aprldpdy289@naver.com>
2024-09-06 15:42:01 +0900

    [docs] Create frontend Project

---
+++ frontend/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { NavigationContainer } from '@react-navigation/native';
+import {AppRegistry} from 'react-native';
+import App from './src/views/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
