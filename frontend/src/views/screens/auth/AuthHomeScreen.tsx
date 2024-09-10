dceb0d3acec86943af530878045fd64fb380077a
zzirco <zzirco@naver.com>
2024-09-10 17:51:17 +0900

    [feat] add AuthHomeScreen

--- frontend/src/views/screens/auth/AuthHomeScreen.tsx
+++ frontend/src/views/screens/auth/AuthHomeScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import {Button, Pressable, SafeAreaView, StyleSheet, View} from 'react-native';
 
-const AuthHomeScreen = ({navigation}) => {
+const AuthHomeScreen = ({navigation}: any) => {
   return (
     <SafeAreaView>
-      <View>
-        <Button
-          title="로그인화면으로 이동"
-          onPress={() => navigation.navigate('Login')}
-        />
-      </View>
+      <Pressable>
+        <View>
+          <Button
+            title="로그인화면으로 이동"
+            onPress={() => navigation.navigate('Login')}
+          />
+        </View>
+      </Pressable>
     </SafeAreaView>
   );
 };
