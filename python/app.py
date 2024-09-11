fe31a8ca0f400af11e491f95d4bf591294c103c7
신순호 <ssh9234@gmail.com>
2024-09-11 16:01:55 +0900

    [feat] 주식 정보 크롤링 완성(금) S11P21A108-147

--- python/app.py
+++ python/app.py
@@ -57,7 +57,7 @@ def fetch_and_send_data(ticker):
             # 전송할 데이터 생성
             data = {
                 "price": float(close_price),
-                "createdAt": adjusted_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                "createdAt": adjusted_timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
             }
             
     # Spring Boot 서버로 데이터 전송
