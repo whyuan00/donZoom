af0c4112c6f0816396e09b6016fa471e62055511
zzirco <zzirco@naver.com>
2024-09-20 16:16:11 +0900

    [feat] Add useAuthStore

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -1,3 +1,25 @@
-import {create} from 'zustand';
+import create from 'zustand';
+import {validateSignup} from '@/utils/validate';
 
-const useAuthStore = create(set => ({}));
+interface SignupState {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  setEmail: (email: string) => void;
+  setPassword: (password: string) => void;
+  setPasswordConfirm: (passwordConfirm: string) => void;
+  validate: () => {email: string; password: string; passwordConfirm: string};
+}
+
+export const useSignupStore = create<SignupState>((set, get) => ({
+  email: '',
+  password: '',
+  passwordConfirm: '',
+  setEmail: email => set({email}),
+  setPassword: password => set({password}),
+  setPasswordConfirm: passwordConfirm => set({passwordConfirm}),
+  validate: () => {
+    const {email, password, passwordConfirm} = get();
+    return validateSignup({email, password, passwordConfirm});
+  },
+}));
