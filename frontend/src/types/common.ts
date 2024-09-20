8789cf4516a6ac84793183b644725d577055e2ef
zzirco <zzirco@naver.com>
2024-09-20 16:14:54 +0900

    [feat] Add types

--- frontend/src/types/common.ts
+++ frontend/src/types/common.ts
@@ -1,4 +1,8 @@
-import {UseMutationOptions} from '@tanstack/react-query';
+import {
+  QueryKey,
+  UseMutationOptions,
+  UseQueryOptions,
+} from '@tanstack/react-query';
 import {AxiosError} from 'axios';
 
 type ResponseError = AxiosError<{
@@ -12,4 +16,9 @@ type UseMutationCustomOptions<TData = unknown, TVariables = unknown> = Omit<
   'mutationFn'
 >;
 
-export type {ResponseError, UseMutationCustomOptions};
+type UseQueryCustomOptions<TQueryFnData = unknown, TData = TQueryFnData> = Omit<
+  UseQueryOptions<TQueryFnData, ResponseError, TData, QueryKey>,
+  'queryKey'
+>;
+
+export type {ResponseError, UseMutationCustomOptions, UseQueryCustomOptions};
