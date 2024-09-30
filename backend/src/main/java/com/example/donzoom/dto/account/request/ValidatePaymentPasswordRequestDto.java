6d7d66fce974c2feb30a15d0227a22ec4c4d1d66
신순호 <ssh9234@gmail.com>
2024-09-30 13:29:39 +0900

    [hotfix] jwtFilter 인증 헤더 없을 시 (로그인) 긴급수정

---
+++ backend/src/main/java/com/example/donzoom/dto/account/request/ValidatePaymentPasswordRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.donzoom.dto.account.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ValidatePaymentPasswordRequestDto {
+  String paymentPassword;
+
+  @JsonCreator
+  public ValidatePaymentPasswordRequestDto(@JsonProperty("paymentPassword") String paymentPassword) {
+    this.paymentPassword = paymentPassword;
+  }
+}
