22423669ca295df0d41e45ba3eda7d45fc787eea
zzirco <zzirco@naver.com>
2024-10-02 13:21:32 +0900

    [feat] Modified to use global state instead of local state

--- frontend/src/hooks/useAccountInfo.ts
+++ frontend/src/hooks/useAccountInfo.ts
@@ -1,55 +1,47 @@
 import {useState, useEffect} from 'react';
 import useAccount from './queries/useAccount';
+import useAccountStore from '@/stores/useAccountStore';
 
 interface AccountBalanceHook {
   account: string;
   balance: string;
   name: string;
-  isLoading: boolean;
   error: Error | null;
   refetch: () => void;
 }
 
 function useAccountBalance(): AccountBalanceHook {
-  const [account, setAccount] = useState('');
-  const [balance, setBalance] = useState('0');
-  const [accountName, setAccountName] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+  const {
+    accountNo: account,
+    balance,
+    name: accountName,
+    setAccountNo: setAccount,
+    setBalance,
+    setName: setAccountName,
+  } = useAccountStore();
   const [error, setError] = useState<Error | null>(null);
   const {getAccount} = useAccount();
 
   const fetchAccount = async () => {
-    setIsLoading(true);
     const accountData = await getAccount.refetch();
-    // console.log('accountData:', accountData.data);
     if (accountData.isSuccess) {
-      console.log('success:', accountData);
       setAccount(accountData.data ? accountData.data.rec[0].accountNo : '');
-      fetchBalance(accountData.data ? accountData.data.rec[0].accountNo : '');
+      setBalance(
+        accountData.data ? accountData.data.rec[0].accountBalance : '',
+      );
       setAccountName(accountData.data ? accountData.data.rec[0].userName : '');
     } else {
-      console.log('failed:', accountData);
       setAccount('');
-      fetchBalance('0');
+      setBalance('0');
       setAccountName('');
     }
-  };
-
-  const fetchBalance = async (accountNo: string) => {
-    const accountData = await getAccount.refetch();
-    setBalance(accountData.data ? accountData.data.rec[0].accountBalance : '');
-    setIsLoading(false);
   };
 
   const refetch = () => {
     fetchAccount();
   };
 
-  useEffect(() => {
-    fetchAccount();
-  }, []);
-
-  return {account, balance, name: accountName, isLoading, error, refetch};
+  return {account, balance, name: accountName, error, refetch};
 }
 
 export default useAccountBalance;
