f80eecee3e968cd0619cc38fc7fb8849444f1432
신순호 <ssh9234@gmail.com>
2024-10-07 13:19:49 +0900

    [feat] Coin 값 불러오기 기능(Get /coin)

---
+++ backend/src/main/java/com/example/donzoom/dto/user/response/CoinResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.donzoom.dto.user.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CoinResponseDto {
+  private final Integer coin;
+}
