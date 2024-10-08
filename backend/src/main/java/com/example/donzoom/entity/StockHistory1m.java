6f0a2f085c6c440355ccffc4da2067d43405811a
신순호 <ssh9234@gmail.com>
2024-10-08 13:13:51 +0900

    [feat] 주식 데이터 주기별 호출 구현 S11P21A108-312

---
+++ backend/src/main/java/com/example/donzoom/entity/StockHistory1m.java
@@ -0,0 +1,52 @@
+package com.example.donzoom.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StockHistory1m {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "stock_history_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "stock_id")
+  private Stock stock;
+
+  private Integer open;
+  private Integer close;
+  private Integer high;
+  private Integer low;
+
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+  @Builder
+  public StockHistory1m(Stock stock, Integer open, Integer close, Integer high, Integer low,
+      LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.stock = stock;
+    this.open = open;
+    this.close = close;
+    this.high = high;
+    this.low = low;
+    this.createdAt = createdAt;
+  }
+}
