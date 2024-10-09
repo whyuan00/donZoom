0f9dfccd732e1610b7ffc0596e116f5f83f1dce1
신순호 <ssh9234@gmail.com>
2024-10-09 17:30:32 +0900

    [fix] 소켓 로깅 추가

--- backend/src/main/java/com/example/donzoom/controller/StockController.java
+++ backend/src/main/java/com/example/donzoom/controller/StockController.java
@@ -14,6 +14,7 @@ import com.example.donzoom.repository.StockHistoryRepository;
 import com.example.donzoom.service.StockService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stock")
@@ -68,7 +70,7 @@ public class StockController {
       @RequestBody StockRequestDto stockRequestDto) {
     Long stockHistoryId = stockService.createStockHistory(stockId, stockRequestDto);
     String interval = stockRequestDto.getInterval();
-
+    log.info(interval);
     // 여기서 WebSocket 전송
     if(interval.equals("1m")) {
       StockHistory1m stockHistory = stockHistory1mRepository.findById(stockHistoryId).orElseThrow();
